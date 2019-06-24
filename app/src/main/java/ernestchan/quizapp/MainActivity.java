acce388d21fd741852cebd2011af9bf02596b23e
student <ernestchan830@gmail.com>
2019-06-24 11:27:20 -0700

    Starting Checkpoint 2 Added True/False Toast Responses

--- app/src/main/java/ernestchan/quizapp/MainActivity.java
+++ app/src/main/java/ernestchan/quizapp/MainActivity.java
@@ -12,8 +12,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private TextView mTextView;
+
     private Button mTrueButton;
     private Button mFalseButton;
+
+    private Question firstQuestion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,12 +29,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mTrueButton.setOnClickListener(this);
         mFalseButton.setOnClickListener(this);
+
+        mTextView = (TextView) findViewById(R.id.text_view);
+
+        firstQuestion = new Question(R.string.question_text1, true);
+        mTextView.setText(firstQuestion.getTextResId());
     }
 
     @Override
     public void onClick(View view) {
         TextView myTextView = (TextView) view;
-        Toast myToast = Toast.makeText(this, "You clicked " + myTextView.getText(), Toast.LENGTH_SHORT);
-        myToast.show();
+        if(view.getId() == R.id.true_button && firstQuestion.getAnswer() == true)
+        {
+            Toast myToast = Toast.makeText(this, "You are correct",  Toast.LENGTH_SHORT);
+            myToast.show();
+        }
+        else if(view.getId() == R.id.false_button && firstQuestion.getAnswer() == false)
+        {
+            Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
+            myToast.show();
+        }
+        else
+            {
+            Toast myToast = Toast.makeText(this, "You are incorrect", Toast.LENGTH_SHORT);
+            myToast.show();
+        }
     }
 }
