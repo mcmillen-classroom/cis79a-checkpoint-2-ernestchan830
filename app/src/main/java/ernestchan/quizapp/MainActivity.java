892e46fbef7fc3979e89856a7eca3b9be7a7508d
ernestchan830@gmail.com <ernestchan830@gmail.com>
2019-06-26 11:54:19 -0700

    get answer

--- app/src/main/java/ernestchan/quizapp/MainActivity.java
+++ app/src/main/java/ernestchan/quizapp/MainActivity.java
@@ -16,8 +16,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
 
-    private Question firstQuestion;
+
+    private Question[] mQuestions;
+    private int mIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,33 +30,48 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mFalseButton =  (Button)  findViewById(R.id.false_button);
+        mNextButton = (Button) findViewById(R.id.next_button);
+
 
         mTrueButton.setOnClickListener(this);
         mFalseButton.setOnClickListener(this);
+        mNextButton.setOnClickListener(this);
+//when next button is clicked next button runs
 
         mTextView = (TextView) findViewById(R.id.text_view);
 
-        firstQuestion = new Question(R.string.question_text1, true);
-        mTextView.setText(firstQuestion.getTextResId());
+        mQuestions = new Question[4];
+        mIndex = 0;
+
+        mQuestions[0] = new Question(R.string.question_text1, true);
+        mQuestions[1] = new Question(R.string.question_text2, true);
+        mQuestions[2] = new Question(R.string.question_text3, true);
+        mQuestions[3] = new Question(R.string.question_text4, true);
+
+        mTextView.setText(mQuestions[mIndex].getTextResId());
     }
 
     @Override
     public void onClick(View view) {
-        TextView myTextView = (TextView) view;
-        if(view.getId() == R.id.true_button && firstQuestion.getAnswer() == true)
-        {
-            Toast myToast = Toast.makeText(this, "You are correct",  Toast.LENGTH_SHORT);
-            myToast.show();
+
+        if (view.getId() == R.id.true_button) {
+            checkAnswer(true);
+        } else if (view.getId() == R.id.false_button) {
+            checkAnswer(false);
+        } else if (view.getId() == R.id.next_button) {
+            mIndex++;
+            mTextView.setText(mQuestions[mIndex].getTextResId());
         }
-        else if(view.getId() == R.id.false_button && firstQuestion.getAnswer() == false)
-        {
-            Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
-            myToast.show();
-        }
-        else
-            {
-            Toast myToast = Toast.makeText(this, "You are incorrect", Toast.LENGTH_SHORT);
-            myToast.show();
+
+        public boolean checkAnswer ( boolean userInput){
+            if (mQuestions[mIndex].getAnswer() == userInput) {
+                Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
+                myToast.show();
+                return true;
+            } else {
+                Toast myToast = Toast.makeText(this, "You are correct", Toast.LENGTH_SHORT);
+                myToast.show();
+                return false;
+            }
         }
     }
-}
